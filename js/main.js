409409475c669902e8ddb81e5b4064e7bf40800d
yunkyung <133613667+Yunkyungii@users.noreply.github.com>
2023-06-07 16:31:15 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -106,10 +106,52 @@ $(function () {
         $('html, body').animate({ scrollTop: 0 }, 600)
     });
 
+    $(window).on('scroll', function () {
+        let sct = $(window).scrollTop();
+        sct > 1000 ? $('.to_top').addClass('on') : $('.to_top').removeClass('on');
+        //show, hide   fadeIn, fadeOut 으로 대체 가능
+    })
+
     $('.history_menu li').on('click', function (e) {
         e.preventDefault();
         let idx = $(this).index();
         $('.history_content li').eq(idx).addClass('on').siblings().removeClass('on');
-    })
+    });
 
+    $('#fl').on('change', function () {
+        const lnk = $(this).val();
+        console.log(lnk, '바뀌네');
+        //val() 이 있을 때만
+        // if (lnk) {
+        //     window.open(lnk);
+        // }; 아래처럼 줄일 수 있음
+        lnk && window.open(lnk);
+    });
+
+
+    $('.family_link2 span').on('click', function () {
+        $(this).toggleClass('on');
+        $(this).next().toggleClass('on');
+    });
+
+    $('.portfolio_slide').on('init afterChange', function (e, s, c) {
+        console.log(c);
+        $('.main_portfolio .itm').eq(c).addClass('on')
+            .siblings().removeClass('on');
+    });
+
+    $('.portfolio_slide').slick({
+        centerMode: true,
+        // centerPadding: "100px",
+        variableWidth: true,
+        arrows: false,
+        dots: true,
+    });
+
+    $('.main_portfolio .tab_arrows .left').on('click', function () {
+        $('.portfolio_slide').slick('slickPrev')
+    });
+    $('.main_portfolio .tab_arrows .right').on('click', function () {
+        $('.portfolio_slide').slick('slickNext')
+    });
 })
